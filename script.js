96a0e94bc534978db8f6fadfd402c321fbdd29c3
Xcharmine <95407796+2003382021@users.noreply.github.com>
2024-02-08 17:16:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+const navBar = document.querySelector('.navigation');
+const menuBar = document.querySelector('.menu');
+const closeIcon = document.querySelector('.close');
+const page = document.querySelector('.iron');
+const updateButton =document.querySelector('#mount');
+
+
+
+updateButton.addEventListener('click', ()=>{
+    page.classList.toggle('active');
+});
+
+menuBar.addEventListener('click', ()=>{
+    navBar.classList.add('active');
+});
+
+closeIcon.addEventListener('click', ()=>{
+    navBar.classList.remove('active');
+});
+
+btnpopup.addEventListener('click', ()=>{
+    navBar.classList.remove('active');
+});
+
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY = 0);
+})
+
